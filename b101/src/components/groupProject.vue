d1b684260a92797254ae9a59af311e9957939c14
Burak Gunduz <burakmgunduz@gmail.com>
2023-01-21 16:38:42 +0000

    try

---
+++ b101/src/components/groupProject.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>Software Engineering Group Project</h3>
+    <canvas></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { basicScene } from '@/groupProject/basicScene';
+export default defineComponent({
+  name: 'groupProject',
+  mounted() {
+    const canvas = document.querySelector("canvas")!;
+    new basicScene(canvas)
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+canvas {
+  width: 70%;
+  height: 70%;
+}
+</style>
